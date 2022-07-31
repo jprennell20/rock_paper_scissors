da9b7b81e88413b7230bfe06e295fb8ea3dfaa49
Josh R <jprennell20@gmail.com>
2022-07-31 18:04:33 -0400

    Add logic and scenarios

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ const choices = ["Rock","Paper","Scissors"]
 //Function to get the computers selection
 function getComputerChoice(){
     // Randomly select 
-    let computerChoice = Math.floor(Math.random()* choices.length);
+    return choices[Math.floor(Math.random()* choices.length)];
 
 }
 
-getComputerChoice();
+let userChoice = prompt('Rock, Paper, or Scissors?')
+
+function playRound(playerSelection, computerSelection) {
+    let computerSelectionUPPER = computerSelection.toUpperCase();
+    let playerSelectionUPPER = playerSelection.toUpperCase();
+    switch(true){
+        case(computerSelectionUPPER === computerSelectionUPPER):
+            return `It's a tie! You both selected: ${playerSelection}`;
+    }
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(computerSelection);
+  console.log(playRound(playerSelection, computerSelection));
