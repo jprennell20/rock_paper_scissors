fd106e95e5788bb20e6ef83e12806ccc354cb6cd
Josh R <jprennell20@gmail.com>
2022-07-31 18:05:10 -0400

    Add logic and scenarios

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 // Array of Choices
 const choices = ["Rock","Paper","Scissors"]
 
-//Function to get the computers selection
+// Function to get the computers selection
 function getComputerChoice(){
-    // Randomly select 
-    return choices[Math.floor(Math.random()* choices.length)];
+    // Randomly selection based on collection size
+    return choices[Math.floor(Math.random() * choices.length)];
 
 }
 
+// Prompt User for Input
 let userChoice = prompt('Rock, Paper, or Scissors?')
 
 function playRound(playerSelection, computerSelection) {
+    // Standardize Value Format
     let computerSelectionUPPER = computerSelection.toUpperCase();
     let playerSelectionUPPER = playerSelection.toUpperCase();
+
+    // Determine the winner
     switch(true){
+        // User Ties with Computer 
         case(computerSelectionUPPER === computerSelectionUPPER):
             return `It's a tie! You both selected: ${playerSelection}`;
+
+        // User Winning Scenarios
+
+        // User Losing Scenarios
     }
   }
-   
+
+  
   const playerSelection = "rock";
   const computerSelection = getComputerChoice();
   console.log(computerSelection);
