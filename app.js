44288c58235484d17dcabfe4d8df181af68ceee7
Josh R <jprennell20@gmail.com>
2022-07-31 17:00:48 -0400

    Create function for computer choice

---
+++ app.js
@@ -0,0 +1,11 @@
+// Array of Choices
+const choices = ["Rock","Paper","Scissors"]
+
+//Function to get the computers selection
+function getComputerChoice(){
+    // Randomly select 
+    let computerChoice = Math.floor(Math.random()* choices.length);
+
+}
+
+getComputerChoice();
