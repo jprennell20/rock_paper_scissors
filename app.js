c617fac277943d99ea1f9bde761fbe16ded075cf
Josh R <jprennell20@gmail.com>
2022-07-31 18:24:24 -0400

    Add winning and losing scenarios

--- app.js
+++ app.js
@@ -3,33 +3,63 @@ const choices = ["Rock","Paper","Scissors"]
 
 // Function to get the computers selection
 function getComputerChoice(){
-    // Randomly selection based on collection size
+    // Randomize selection based on collection size
     return choices[Math.floor(Math.random() * choices.length)];
 
 }
-
-// Prompt User for Input
-let userChoice = prompt('Rock, Paper, or Scissors?')
 
 function playRound(playerSelection, computerSelection) {
     // Standardize Value Format
     let computerSelectionUPPER = computerSelection.toUpperCase();
     let playerSelectionUPPER = playerSelection.toUpperCase();
 
+    console.log(playerSelectionUPPER);
+    console.log(computerSelectionUPPER);
+
+    // Generate Standard Response Messages
+    let winnerMessage = `You Win! ${playerSelection} beats ${computerSelection}`;
+    let loserMessage = `You Lose! ${computerSelection} beats ${playerSelection}`;
+
     // Determine the winner
     switch(true){
         // User Ties with Computer 
-        case(computerSelectionUPPER === computerSelectionUPPER):
+        case(playerSelectionUPPER === computerSelectionUPPER):
             return `It's a tie! You both selected: ${playerSelection}`;
 
         // User Winning Scenarios
+        case(playerSelectionUPPER ==="ROCK" && computerSelectionUPPER === "SCISSORS"):
+            return winnerMessage;
+
+        case(playerSelectionUPPER ==="SCISSORS" && computerSelectionUPPER === "PAPER"):
+            return winnerMessage;
+
+        case(playerSelectionUPPER ==="PAPER" && computerSelectionUPPER === "ROCK"):
+            return winnerMessage;
+
 
         // User Losing Scenarios
+        case(computerSelectionUPPER ==="ROCK" && playerSelectionUPPER === "SCISSORS"):
+            return loserMessage;
+
+        case(computerSelectionUPPER ==="SCISSORS" && playerSelectionUPPER === "PAPER"):
+            return loserMessage;
+
+        case(computerSelectionUPPER ==="PAPER" && playerSelectionUPPER === "ROCK"):
+            return loserMessage;
+
+        default:
+            return "Please provide a valid selection.";
     }
   }
 
-  
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
+
+// Prompt User for Input
+let playerSelection = prompt('Rock, Paper, or Scissors?')
+
+// Call function to get computer selection
+const computerSelection = getComputerChoice();
+
+// Log Result
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
