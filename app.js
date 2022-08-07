40c73d87364d30143468b2d4759f00fb45cdfc15
Josh R <jprennell20@gmail.com>
2022-08-07 19:10:51 -0400

    Build User Interface

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-// Array of Choices
+// Initialize Variables
 const choices = ["Rock","Paper","Scissors"]
+let userScore = 0;
+let computerScore = 0;
 
 // Function to get the computers selection
 function getComputerChoice(){
@@ -7,6 +9,25 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random() * choices.length)];
 
 }
+
+// Add Event Listener to Rock to Initiate a Round
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+  playRound("Rock",getComputerChoice());
+});
+
+// Add Event Listener to Paper to Initiate a Round
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+  playRound("Paper",getComputerChoice());
+});
+
+// Add Event Listener to Scissors to Initiate a Round
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+  playRound("Scissors",getComputerChoice());
+});
 
 // Helper class to determine if the user won
 function isWinner(playerSelectionUPPER,computerSelectionUPPER){
@@ -58,46 +79,63 @@ function playRound(playerSelection, computerSelection) {
     // Determine the winner
     switch(true){
         // User ties computer 
-        case(playerSelectionUPPER === computerSelectionUPPER):
-            return tieMessage;
+        case(playerSelectionUPPER === computerSelectionUPPER): 
+            console.log(tieMessage);
+            break;
 
         // User beats computer
         case(isWinner(playerSelectionUPPER, computerSelectionUPPER) == true):
-            return winnerMessage;
+            // Increment user score and update score display 
+            ++userScore;
+            let userScoreDisplay = document.getElementById('user-score');
+            userScoreDisplay.textContent = `User: ${userScore}`;
+            console.log(winnerMessage);
+            break;
         
         // Computer beats user
         case(isLoser(computerSelectionUPPER, playerSelectionUPPER) == true):
-            return loserMessage;
+            // Increment user score and update score display
+            ++computerScore;
+            let compScoreDisplay = document.getElementById('comp-score');
+            compScoreDisplay.textContent = `Computer: ${computerScore}`;
+            console.log(loserMessage);
+            break;
 
         // User supplies invalid selection
         default:
-            return "Please provide a valid selection.";
+            alert("Please provide a valid selection.");
+    }
+
+    if(userScore >= 5){
+        alert("You've Defeated the Computer!!")
+    }else if(computerScore >= 5){
+        alert("You've Been Crushed by The Computer...");
     }
   }
 
-function game(){
-    let userScore = 0;
-    let computerScore = 0;
-    for(let i = 0; i < 5; i++){
-        // Prompt User for Input
-        let playerSelection = prompt('Rock, Paper, or Scissors?');
+// function game(){
+//     let userScore = 0;
+//     let computerScore = 0;
+//     for(let i = 0; i < 5; i++){
+//         // Prompt User for Input
+//         let playerSelection = prompt('Rock, Paper, or Scissors?');
 
-        // Call function to get computer selection
-        const computerSelection = getComputerChoice();
+//         // Call function to get computer selection
+//         const computerSelection = getComputerChoice();
 
-        let result = playRound(playerSelection,computerSelection);
+//         let result = playRound(playerSelection,computerSelection);
 
-        if (result.search("Win") != -1){
-            userScore++;
-        }else if(result.search("Lose") != -1){
-            computerScore++;
-        }
-        // Log Result
-        console.log(playerSelection);
-        console.log(computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`User: ${userScore} vs Computer: ${computerScore}`)
-    }
-}
+//         if (result.search("Win") != -1){
+//             userScore++;
+//         }else if(result.search("Lose") != -1){
+//             computerScore++;
+//         }
+//         // Log Result
+//         console.log(playerSelection);
+//         console.log(computerSelection);
+//         console.log(playRound(playerSelection, computerSelection));
+//         console.log(`User: ${userScore} vs Computer: ${computerScore}`)
+//     }
+// }
 
-game();
+// game();
